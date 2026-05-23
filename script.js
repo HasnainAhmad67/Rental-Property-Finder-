e47001997917f894043635b237ab01cc34709deb
HasnainAhmad67 <hasnainahmad6718@gmail.com>
2026-05-23 20:47:29 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Rental Property Finder Loaded");
+
+
+
+const buttons =
+document.querySelectorAll(".card button");
+
+
+
+buttons.forEach(function(button){
+
+    button.addEventListener("click", function(){
+
+        alert("Property Details Coming Soon!");
+
+    });
+
+});
